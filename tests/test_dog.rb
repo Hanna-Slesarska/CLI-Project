752f6789b35b313dbe757f8f35ae792b9cd9485e
Hanna Slesarska <127249050+Hanna-Slesarska@users.noreply.github.com>
2024-05-11 19:55:44 +0000

    submited minitest

--- tests/test_dog.rb
+++ tests/test_dog.rb
@@ -1,2 +1,24 @@
 require 'minitest/autorun'
-require './app'
+require_relative '../dog'
+
+class DogTest < Minitest::Test
+  def setup
+    @dog = Dog.new("Buddy", "Golden Retriever", 3)
+  end
+
+  def test_dog_name
+    assert_equal "Buddy", @dog.name
+  end
+
+  def test_dog_breed
+    assert_equal "Golden Retriever", @dog.breed
+  end
+
+  def test_dog_age
+    assert_equal 3, @dog.age
+  end
+
+  def test_to_s
+    assert_equal "Buddy - Golden Retriever, 3 years old", @dog.to_s
+  end
+end
